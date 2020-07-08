d26546986abea5df5550326b85e5d458e8ae413a
Laura Davis <laura.l.davis.210@gmail.com>
2020-07-08 10:50:13 -0500

    MySQL Clauses - Where - Exercise Part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,22 +2,29 @@ USE employees;
 
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = 'Irena' OR
+      first_name = 'Vidya' OR
+      first_name = 'Maya')
+AND gender = 'M';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE last_name LIKE 'E%E';
 
 SELECT *
 FROM employees
-WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND MONTH(birth_date) = 12
+  AND DAY(birth_date) = 25;
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
 -- alternate solution
--- WHERE last_name REGEXP 'q';
+-- WHERE last_name REGEXP 'q' AND last_name NOT REGEXP 'qu';
