f970a50f8802c92f87bd08a8192389d71e9182ba
Laura Davis <laura.l.davis.210@gmail.com>
2020-07-09 10:23:19 -0500

    MySQL Group By and Aliases Exercises

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,16 @@
 USE employees;
 
+-- 3.
+SELECT CONCAT(last_name, ', ',  first_name) AS full_name
+FROM employees
+LIMIT 10;
+
+-- 4.
+SELECT CONCAT(last_name, ', ',  first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+-- 5.
 SELECT CONCAT(emp_no, ' - ', last_name, ', ',  first_name) AS full_name, birth_date AS DOB
 FROM employees
 LIMIT 10;
--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,22 +1,27 @@
 USE employees;
 
+-- 2.
 SELECT DISTINCT title FROM titles;
 
+-- 3.
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
 GROUP BY last_name;
 
+-- 4.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
 GROUP BY first_name, last_name;
 
+-- 5.
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%'
   AND last_name NOT LIKE '%qu%';
 
+-- 6.
 SELECT last_name, COUNT(*) AS count
 FROM employees
 WHERE last_name LIKE '%q%'
@@ -24,6 +29,7 @@ WHERE last_name LIKE '%q%'
 GROUP BY last_name
 ORDER BY last_name;
 
+-- 7.
 SELECT count(*) AS count, gender
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
